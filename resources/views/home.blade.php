3126e3bdced78ef7408316f53d62b6ed8937bb95
bilazahirah <bilazahirah.13@gmail.com>
2025-04-15 00:34:06 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div classname="container">
+        <h1 class="">Welcome to the Home Page</h1>
+        <p>This is a simple Laravel Blade template.</p>
+    </div>
+</body>
+</html>
